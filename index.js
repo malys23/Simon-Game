ffcfc74a18fcda24ddf147681bb1aa893dc96c04
Alyona M <alyona.maliassova@gmail.com>
2025-03-12 18:33:07 -0500

    Updated code to play sound at chosen button

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ var gamePattern = [];
 //function to generate number from  0 to 3
 function nextSequence(){
     var randomNumber = Math.floor(Math.random() * 4);
-    console.log(randomNumber);
+
+    //choose color based on random number
+    var randomChosenColour = buttonColors[randomNumber];
+    //push new color into gamePattern
+    gamePattern = gamePattern.push(randomChosenColour);
+
+    //to make specified random color to flash
+    $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
+    //to make specified random button play sound when flashing
+    var audio = new Audio(`./sounds/${randomChosenColour}.mp3`)
+    audio.play();
 }
 
-//choose color based on random number
-var randomChosenColour = buttonColors[nextSequence()];
-//push new color into gamePattern
-gamePattern = gamePattern.push(randomChosenColour);
-
-//to make specified random color to flash
-$("#" + randomChosenColour).fadeOut(100).fadeIn(100);
-//to make specified random button play sound when flashing
